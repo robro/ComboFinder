e602129ca64c1d59c8d7cd53508bb83aa1e0aeb7
Ian Brock <pibborando@gmail.com>
2023-05-10 16:46:59 -0700

    ✨ Add filters window

--- combofinder.py
+++ combofinder.py
@@ -58,7 +58,21 @@ class Combo:
 
 class ComboFinder(tk.Tk):
     combos: list[Combo] = []
-    combo_props = ('Character', 'Notation', 'Startup', 'Drive', 'Super', 'Carry', 'Damage', 'Advantage')
+    combo_props = (
+        'Character',
+        'Notation',
+        'Startup',
+        'Drive',
+        'Super',
+        'Carry',
+        'Damage',
+        'Advantage'
+    )
+    compares = (
+        'Equals',
+        'Greater Than',
+        'Less Than'
+    )
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -68,53 +82,110 @@ class ComboFinder(tk.Tk):
         button_width = 20
         title_font = ('Calibri', 12)
         row_count = 10
+        header_bg = 'gray'
+        text_bgs = ('white', 'lightgray')
+        
+        self.filters = {prop: [tk.StringVar(value=self.compares[0]), tk.StringVar()] for prop in self.combo_props}
 
-        self.title('Combo Finder')
+        self.title('SF6 Combo Finder')
         self.resizable(False, False)
 
-        self.btns = tk.Frame(self, padx=padx, pady=pady)
-        self.btns.grid(row=0, sticky='nesw')
+        # Button Frame
 
-        self.import_btn = ttk.Button(self.btns, text='Import combos', width=button_width, command=self.import_combos)
-        self.import_btn.grid(row=0, column=0, padx=padx, pady=pady, sticky='w')
-        self.filter_btn = ttk.Button(self.btns, text='Filter', width=button_width)
-        self.filter_btn.grid(row=0, column=1, padx=15, pady=pady, sticky='w')
+        self.button_frame = tk.Frame(self, padx=padx, pady=pady)
+        self.button_frame.grid(row=0, sticky='nesw')
 
-        self.main = tk.Frame(self, padx=10, pady=10)
+        ttk.Button(self.button_frame, text='Import combos', width=button_width, command=self.import_combos)\
+            .grid(row=0, column=0, padx=padx, pady=pady, sticky='w')
+        ttk.Button(self.button_frame, text='Filters', width=button_width, command=self.open_filters)\
+            .grid(row=0, column=1, padx=15, pady=pady, sticky='w')
+
+        # Info Frame
+
+        self.info_frame = tk.Frame(self, padx=10, pady=10)
         column_sizes = (100, 200, 100, 100, 100, 100, 100, 100)
         for i, size in enumerate(column_sizes):
-            self.main.grid_columnconfigure(i, minsize=size)
-        self.main.grid(row=1)
-        self.labels = [ttk.Label(self.main, text=column_title, background='gray', anchor='center', padding=padx, font=title_font) for column_title in self.combo_props]
-        for i, label in enumerate(self.labels):
+            self.info_frame.grid_columnconfigure(i, minsize=size)
+        self.info_frame.grid(row=1)
+        headers = [ttk.Label(self.info_frame, text=header_str, background=header_bg, anchor='center', padding=padx, font=title_font)
+                   for header_str in self.combo_props]
+        for i, label in enumerate(headers):
             label.grid(row=0, column=i, sticky='nsew')
 
-        self.text_rows = [[tk.Text(self.main, width=1, height=1, border=0, cursor='arrow', padx=padx, pady=pady) for _ in range(len(self.combo_props))] for _ in range(row_count)]
+        self.text_rows = [[
+            tk.Text(self.info_frame, width=1, height=1, border=0, cursor='arrow', padx=padx, pady=pady)
+            for _ in range(len(self.combo_props))]
+            for _ in range(row_count)]
         for i, text_row in enumerate(self.text_rows, 1):
-            bg_color = 'white' if i % 2 else 'lightgray'
+            bg_color = text_bgs[0] if i % 2 else text_bgs[1]
             for j, text_box in enumerate(text_row):
                 # text_box.insert('end', f'{i}{j}')
                 text_box.config(bg=bg_color)
                 text_box.grid(row=i, column=j, sticky='nsew')
                 text_box.config(state='disabled')
 
+        # Message Frame
+
         self.message_frame = tk.Frame(self)
         self.message_frame.grid(row=2, sticky='nsew')
         self.message_var = tk.StringVar()
-        self.message = tk.Message(self.message_frame, textvariable=self.message_var, width=300)
-        self.message.grid()
+        tk.Message(self.message_frame, textvariable=self.message_var, width=300).grid(sticky='w')
+
+    def open_filters(self):
+        try:
+            self.filter_window.deiconify()
+            return
+        except:
+            pass
+
+        self.filter_window = tk.Toplevel(self)
+        self.filter_window.title('Filters')
+        self.filter_window.resizable(False, False)
+
+        # Filters frame
+
+        self.filters_frame = tk.Frame(self.filter_window, padx=5, pady=5)
+        self.filters_frame.grid(row=0)
+        
+        for i, prop in enumerate(self.combo_props):
+            entry_width = 30
+            columnspan = 2
+            column = 1
+            padx = 5
+            ttk.Label(self.filters_frame, text=prop, anchor='center', padding=5)\
+                .grid(row=i, column=0, sticky='w')
+            if i > 1:
+                entry_width -= 16
+                columnspan = 1
+                column = 2
+                padx = 0
+                ttk.Combobox(self.filters_frame, width=11, textvariable=self.filters[prop][0], values=self.compares)\
+                    .grid(row=i, column=1, sticky='w', padx=5, pady=5)
+            ttk.Entry(self.filters_frame, width=entry_width, textvariable=self.filters[prop][1])\
+                .grid(row=i, column=column, sticky='w', padx=padx, pady=5, columnspan=columnspan)
+        
+        # Buttons frame
+
+        self.submit_frame = tk.Frame(self.filter_window, padx=5, pady=5)
+        self.submit_frame.grid(row=1)
+
+        ttk.Button(self.submit_frame, text='Submit', command=self.update_filters)\
+            .grid(row=0, column=0, sticky='w')
 
     def import_combos(self):
         filename = filedialog.askopenfilename(initialdir=os.path.abspath('.'), filetypes=[('Comma separated', '.csv')])
         if not filename:
             return
-        
+
         with open(filename, 'r') as combos_file:
             combos_list = list(csv.reader(combos_file, skipinitialspace=True))[1:]
 
         self.combos = list(set(Combo(*combo) for combo in combos_list))
         self.update_display()
         self.message_var.set('Combos successfully imported')
+
+    def update_filters(self):
+        [print(name, text[0].get(), text[1].get()) for name, text in self.filters.items()]
 
     def update_display(self):
         for i, row in enumerate(self.text_rows):
