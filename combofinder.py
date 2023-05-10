0a4f058969fef0410ad8b64e10dca1c7d5b76b2d
Ian Brock <pibborando@gmail.com>
2023-05-10 00:15:01 -0700

    ✨ Add initial gui stuff

--- combofinder.py
+++ combofinder.py
@@ -2,7 +2,12 @@
 import csv
 import operator
 import argparse
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
 from dataclasses import dataclass
+
 
 @dataclass(frozen=True)
 class Combo:
@@ -60,6 +65,46 @@ CAR: {self.carry:.0%}\n\
 DMG: {self.damage}\n\
 ADV: {"+" if int(self.advantage) >= 0 else ""}{self.advantage}\n'
 
+
+class ComboFinder(tk.Tk):
+    combos: set = None
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title('Combo Finder')
+        self.resizable(False, False)
+
+        self.frame = tk.Frame(self, padx=10, pady=10)
+        self.frame.pack()
+
+        self.combo_import_btn = ttk.Button(self.frame, text='Import combos', width=40, command=self.import_combos)
+        self.combo_import_btn.grid(row=0, column=0, padx=5, pady=5)
+
+        self.text = tk.Text(self.frame, relief='flat', cursor='arrow', width=40, state='disabled')
+        self.text.grid(row=1, column=0, padx=5, pady=5)
+
+        self.msg_var = tk.StringVar()
+        self.message = tk.Message(self.frame, textvariable=self.msg_var, width='300p')
+        self.message.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+
+    def import_combos(self):
+        filename = filedialog.askopenfilename(initialdir=os.path.abspath('.'), filetypes=[('CSV', '.csv')])
+
+        with open(filename, 'r') as combos_file:
+            combos_list = list(csv.reader(combos_file, skipinitialspace=True))[1:]
+
+        self.combos = set(Combo(*combo) for combo in combos_list)
+        self.update_combos()
+        self.msg_var.set('Combos successfully imported')
+
+    def update_combos(self):
+        self.text['state'] = 'normal'
+        self.text.delete(1.0, 'end')
+        self.text.insert('end', '\n'.join(combo.__str__() for combo in self.combos))
+        self.text['state'] = 'disabled'
+
+
 def get_truth(inp, relate, cut):
     ops = {'gt': operator.gt,
            'lt': operator.lt,
@@ -112,4 +157,5 @@ def main() -> None:
     [print(combo) for combo in combos]
 
 if __name__ == "__main__":
-    main()
+    combo_finder = ComboFinder()
+    combo_finder.mainloop()
