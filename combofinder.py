42228f51b78b86f0d5d7eebe89f6decf369328b2
Ian Brock <pibborando@gmail.com>
2023-05-12 17:43:29 -0700

    🔧 Do some minor refactoring

--- combofinder.py
+++ combofinder.py
@@ -85,14 +85,14 @@ class ComboFilter:
 
 class ComboFinder(tk.Tk):
 
-    default_pad = 5
+    padding = 5
     combos_per_page = 10
-    current_sort = 'character'
+    current_sort = ''
     is_reversed = False
     combos: list[Combo] = []
     display_combos: list[Combo] = []
 
-    compares = (
+    operators = (
         'Equals',
         'Greater Than',
         'Less Than'
@@ -118,27 +118,28 @@ class ComboFinder(tk.Tk):
         self.filters = (
             ComboFilter('Character', tk.StringVar(value='Contains'), tk.StringVar(), 'str', 100),
             ComboFilter('Notation', tk.StringVar(value='Contains'), tk.StringVar(), 'str', 200),
-            ComboFilter('Startup', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'int', 100, options=self.compares),
-            ComboFilter('Drive', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'float', 100, options=self.compares),
-            ComboFilter('Super', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'int', 100, options=self.compares),
-            ComboFilter('Carry', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'int', 100, options=self.compares),
-            ComboFilter('Damage', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'int', 100, options=self.compares),
-            ComboFilter('Advantage', tk.StringVar(value=self.compares[0]), tk.StringVar(), 'int', 100, options=self.compares),
-            ComboFilter('Position', tk.StringVar(value=self.compares[0]), tk.StringVar(value=self.positions[0]), 'str', 100,
+            ComboFilter('Startup', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'int', 100, options=self.operators),
+            ComboFilter('Drive', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'float', 100, options=self.operators),
+            ComboFilter('Super', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'int', 100, options=self.operators),
+            ComboFilter('Carry', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'int', 100, options=self.operators),
+            ComboFilter('Damage', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'int', 100, options=self.operators),
+            ComboFilter('Advantage', tk.StringVar(value=self.operators[0]), tk.StringVar(), 'int', 100, options=self.operators),
+            ComboFilter('Position', tk.StringVar(value=self.operators[0]), tk.StringVar(value=self.positions[0]), 'str', 100,
                         options=self.positions, has_entry=False),
         )
         self.labels = [filter.label for filter in self.filters]
 
-        # Button frame
+        # Top frame
 
-        self.button_frame = tk.Frame(self, padx=self.default_pad, pady=self.default_pad)
-        self.button_frame.grid(row=0, sticky='nesw')
+        self.top_frame = tk.Frame(self, padx=self.padding, pady=self.padding)
+        self.top_frame.grid(row=0, sticky='nesw')
 
         button_width = 15
-        ttk.Button(self.button_frame, text='Import combos', width=button_width, command=self.import_combos)\
-            .grid(row=0, column=0, padx=self.default_pad, pady=self.default_pad)
-        ttk.Button(self.button_frame, text='Filters', width=button_width, command=self.open_filters)\
-            .grid(row=0, column=1, padx=self.default_pad, pady=self.default_pad)
+
+        ttk.Button(self.top_frame, text='Import combos', width=button_width, command=self.import_combos)\
+            .grid(row=0, column=0, padx=self.padding, pady=self.padding)
+        ttk.Button(self.top_frame, text='Filters', width=button_width, command=self.open_filters)\
+            .grid(row=0, column=1, padx=self.padding, pady=self.padding)
 
         # Combo frame
 
@@ -146,17 +147,15 @@ class ComboFinder(tk.Tk):
 
         # Bottom frame
 
-        self.bottom_frame = tk.Frame(self, padx=self.default_pad, pady=self.default_pad)
-        self.bottom_frame.columnconfigure(0, weight=1)
-        self.bottom_frame.columnconfigure(1, weight=1)
+        self.bottom_frame = tk.Frame(self, padx=self.padding, pady=self.padding)
         self.bottom_frame.grid(row=3, sticky='nsew')
 
         self.message_var = tk.StringVar()
         tk.Message(self.bottom_frame, textvariable=self.message_var, width=300)\
-            .pack(side='left', pady=self.default_pad)
+            .pack(side='left', pady=self.padding)
         
         self.page_up_btn = ttk.Button(self.bottom_frame, text='>', width=3, command=self.page_up)
-        self.page_up_btn.pack(side='right', padx=self.default_pad)
+        self.page_up_btn.pack(side='right', padx=self.padding)
 
         self.page_entry = ttk.Entry(self.bottom_frame, width=3, textvariable=self.current_page)
         self.page_entry.pack(side='right')
@@ -164,17 +163,18 @@ class ComboFinder(tk.Tk):
         self.page_entry.config(state='disabled')
 
         self.page_down_btn = ttk.Button(self.bottom_frame, text='<', width=3, command=self.page_down)
-        self.page_down_btn.pack(side='right', padx=self.default_pad)
+        self.page_down_btn.pack(side='right', padx=self.padding)
 
         page_sizes = (5, 10, 20)
         page_box_values = [f'{size} per page' for size in page_sizes]
+
         self.per_page_var = tk.StringVar(value=page_box_values[1])
         self.per_page_box = ttk.Combobox(self.bottom_frame, width=12, textvariable=self.per_page_var, values=page_box_values, state='readonly')
-        self.per_page_box.pack(side='right', padx=self.default_pad)
+        self.per_page_box.pack(side='right', padx=self.padding)
         self.per_page_box.bind('<<ComboboxSelected>>', self.update_combo_frame)
-        ttk.Label(self.bottom_frame, text='Show:').pack(side='right', padx=self.default_pad)
+        ttk.Label(self.bottom_frame, text='Show:').pack(side='right', padx=self.padding)
 
-        self.sort_by(self.current_sort)
+        self.sort_by('character')
 
     def create_combo_frame(self):
         try:
@@ -182,7 +182,7 @@ class ComboFinder(tk.Tk):
         except AttributeError:
             pass
 
-        self.combo_frame = tk.Frame(self, padx=10, pady=self.default_pad)
+        self.combo_frame = tk.Frame(self, padx=10, pady=self.padding)
         for column, size in enumerate([prop.column_size for prop in self.filters]):
             self.combo_frame.grid_columnconfigure(column, minsize=size)
         self.combo_frame.grid(row=1, sticky='nsew')
@@ -195,7 +195,7 @@ class ComboFinder(tk.Tk):
             header.config(state='disabled')
 
         self.combo_rows = [[
-            ttk.Label(self.combo_frame, padding=self.default_pad) for _ in range(len(self.filters))]
+            ttk.Label(self.combo_frame, padding=self.padding) for _ in range(len(self.filters))]
             for _ in range(self.combos_per_page)]
         current_row = 1
         for row in self.combo_rows:
@@ -205,8 +205,11 @@ class ComboFinder(tk.Tk):
                 .grid(row=current_row+1, columnspan=len(self.filters), sticky='nsew')
             current_row += 2
 
-    def update_combo_frame(self, event=tk.Event()):
-        self.combos_per_page = int(event.widget.get().split()[0])
+    def update_combo_frame(self, event: tk.Event):
+        new_value = int(event.widget.get().split()[0])
+        if self.combos_per_page == new_value:
+            return
+        self.combos_per_page = new_value
         self.create_combo_frame()
         self.update_display()
 
@@ -223,36 +226,36 @@ class ComboFinder(tk.Tk):
 
         # Filters frame
 
-        self.filters_frame = tk.Frame(self.filter_window, padx=self.default_pad, pady=self.default_pad)
+        self.filters_frame = tk.Frame(self.filter_window, padx=self.padding, pady=self.padding)
         self.filters_frame.grid(row=0)
 
         full_width = 33
         part_width = 15
 
-        for i, prop in enumerate(self.filters):
-            ttk.Label(self.filters_frame, text=prop.label, anchor='center', padding=self.default_pad)\
+        for i, filter in enumerate(self.filters):
+            ttk.Label(self.filters_frame, text=filter.label, anchor='center', padding=self.padding)\
                 .grid(row=i, column=0, sticky='w')
-            if prop.options:
-                textvar = prop.compare if prop.has_entry else prop.value
-                ttk.Combobox(self.filters_frame, width=part_width-3, textvariable=textvar, values=prop.options, state='readonly')\
-                    .grid(row=i, column=1, sticky='w', padx=self.default_pad, pady=self.default_pad)
-            if prop.has_entry:
-                width = part_width if prop.options else full_width
-                column = 2 if prop.options else 1
-                columnspan = 1 if prop.options else 2
-                sticky = 'e' if prop.options else 'w'
-                ttk.Entry(self.filters_frame, width=width, textvariable=prop.value)\
-                    .grid(row=i, column=column, sticky=sticky, padx=self.default_pad, pady=self.default_pad, columnspan=columnspan)
+            if filter.options:
+                textvar = filter.compare if filter.has_entry else filter.value
+                ttk.Combobox(self.filters_frame, width=part_width-3, textvariable=textvar, values=filter.options, state='readonly')\
+                    .grid(row=i, column=1, sticky='w', padx=self.padding, pady=self.padding)
+            if filter.has_entry:
+                width = part_width if filter.options else full_width
+                column = 2 if filter.options else 1
+                columnspan = 1 if filter.options else 2
+                sticky = 'e' if filter.options else 'w'
+                ttk.Entry(self.filters_frame, width=width, textvariable=filter.value)\
+                    .grid(row=i, column=column, sticky=sticky, padx=self.padding, pady=self.padding, columnspan=columnspan)
 
         # Buttons frame
 
-        self.submit_frame = tk.Frame(self.filter_window, padx=self.default_pad, pady=self.default_pad)
+        self.submit_frame = tk.Frame(self.filter_window, padx=self.padding, pady=self.padding)
         self.submit_frame.grid(row=1)
 
         ttk.Button(self.submit_frame, text='Submit', command=self.update_combos)\
-            .grid(row=0, column=0, padx=self.default_pad, pady=self.default_pad, sticky='w')
+            .grid(row=0, column=0, padx=self.padding, pady=self.padding, sticky='w')
         ttk.Button(self.submit_frame, text='Reset', command=self.reset_filters)\
-            .grid(row=0, column=1, padx=self.default_pad, pady=self.default_pad, sticky='w')
+            .grid(row=0, column=1, padx=self.padding, pady=self.padding, sticky='w')
 
     def import_combos(self):
         filename = filedialog.askopenfilename(initialdir=os.path.abspath('.'), filetypes=[('Comma separated', '.csv')])
